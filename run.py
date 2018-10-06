b20b58f3dc09b1e867392a62cc4e1cbdd5990c1f
Bruce <brucetushabe@gmail.com>
2018-10-06 22:10:41 +0300

    These are being committed. Hope i understand clearly

---
+++ run.py
@@ -0,0 +1,3 @@
+from todo import Todo
+
+todo = Todo
---
+++ todo.py
@@ -0,0 +1,23 @@
+class todo:
+    def __init__(self,name,created_at ):
+        
+        self.name = name 
+        self.created_at = created_at
+        self.list()
+        
+
+    def add_task (self, name):
+        self.append = list
+        
+    def show_tasks(self, task):
+        return 
+
+    def delete_task(self, delete_task):
+        self.delete_task = delete_task
+    
+class HomeTodo:
+    def_
+
+
+    
+
